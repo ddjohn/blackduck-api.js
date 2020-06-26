55cf6f9d31b761ec2209be079fd0159a24c17468
David Johansson <david.johansson@aptiv.com>
2020-06-27 01:06:37 +0200

    Stubbing blackduck for bearer tests

--- test/test.js
+++ test/test.js
@@ -3,12 +3,31 @@ const log = debug('test.js');
 
 log('loading...');
 
-import { BlackDuckAPI } from './index.js';
-import { API_TOKEN, API_URL } from './config.js';
+import { BlackDuckAPI } from '../index.js';
+import { API_TOKEN, API_URL } from '../config.js';
 import assert from 'assert';
+import sinon from 'sinon';
+import got from 'got';
+
+after(function () {
+    sinon.restore();
+});
 
 describe('Bearer Test', () => {
+    let sandbox = null;
+
+    beforeEach(() => {
+      sandbox = sinon.createSandbox();
+    });
+  
+    afterEach(() => {
+      sandbox.restore()
+    });
+
     it('Valid bearer', (done) => {
+        const json = JSON.parse('{}');
+        json["body"] = '{"bearerToken":"Hello"}';
+        sandbox.stub(got, 'post').returns(json);
 
         const bd = new BlackDuckAPI(API_URL, API_TOKEN);
         const bearer = bd.getBearer().then((bearer) => {
@@ -17,7 +36,9 @@ describe('Bearer Test', () => {
         });
     });
 
-    it('Valid bearer due to incorrect token', (done) => {
+    it('Invalid bearer due to incorrect token', (done) => {
+        sandbox.stub(got, 'post').throws();
+
         const bd = new BlackDuckAPI(API_URL, 'wrong_token');
         const bearer = bd.getBearer().then((bearer) => {
             assert.strictEqual(bearer, undefined);
@@ -25,6 +46,7 @@ describe('Bearer Test', () => {
         });
     });
 
+    /*
     it('Valid bearer due to missing black duck', (done) => {
         const bd = new BlackDuckAPI("http://www.google.com/", API_TOKEN);
         const bearer = bd.getBearer().then((bearer) => {
@@ -40,5 +62,6 @@ describe('Bearer Test', () => {
             done();
         });
     });
+    */
 });
 
