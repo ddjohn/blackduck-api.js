1852634942888e5af644a5f7a8435f854d8b5c08
David Johansson <david.johansson@aptiv.com>
2020-06-27 09:24:11 +0200

    Adding testcases for bearer and projects

---
+++ test/test-bearer.js
@@ -0,0 +1,45 @@
+import debug from 'debug';
+const log = debug('test-bearer.js');
+
+log('loading...');
+
+import { BlackDuckAPI } from '../index.js';
+import { API_TOKEN, API_URL } from '../config.js';
+import assert from 'assert';
+import sinon from 'sinon';
+import got from 'got';
+
+describe('Bearer Test', () => {
+  let sandbox = null;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+  
+  afterEach(() => {
+    sandbox.restore()
+  });
+
+  it('Valid bearer', (done) => {
+    const json = JSON.parse('{}');
+    json["body"] = '{"bearerToken":"Hello"}';
+    sandbox.stub(got, 'post').returns(json);
+
+    const bd = new BlackDuckAPI(API_URL, API_TOKEN);
+    const bearer = bd.getBearer().then((bearer) => {
+      assert.ok(bearer);
+      assert.strictEqual("Hello", bearer.bearerToken);
+      done();
+    });
+  });
+
+  it('Invalid bearer due to incorrect token', (done) => {
+    sandbox.stub(got, 'post').throws();
+
+    const bd = new BlackDuckAPI(API_URL, 'wrong_token');
+    const bearer = bd.getBearer().then((bearer) => {
+      assert.strictEqual(bearer, undefined);
+      done();
+    });
+  });
+});
---
+++ test/test-project.js
@@ -0,0 +1,39 @@
+import debug from 'debug';
+const log = debug('test-project.js');
+
+log('loading...');
+
+import { BlackDuckAPI } from '../index.js';
+import { API_TOKEN, API_URL } from '../config.js';
+import assert from 'assert';
+import sinon from 'sinon';
+import got from 'got';
+
+describe('Projects Test', () => {
+  let sandbox = null;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+  
+  afterEach(() => {
+    sandbox.restore()
+  });
+
+  it('List 3 projects', (done) => {
+    const json = JSON.parse('{}');
+    json["body"] = '{"totalCount":3, "items":[{"name":"a"},{"name":"b"},{"name":"c"}]}';
+    sandbox.stub(got, 'get').returns(json);
+
+    const bd = new BlackDuckAPI(API_URL, API_TOKEN);
+    const projects = bd.getProjects('').then((projects) => {
+      assert.ok(projects);
+
+      projects.forEach((project)  => {
+        assert.ok(project);
+        assert.ok(project.name); 
+      });
+      done();
+    });
+  });
+});
